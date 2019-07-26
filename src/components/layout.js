40b272f3634d651f9264cb8849ed510215bed9b8
uf0 <scnepa222@gmail.com>
2019-07-26 12:13:57 +0200

    added cookies consent

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
+import CookieConsent from "react-cookie-consent"
 import styles from "./layout.module.scss"
 
 const Layout = ({ children, headerBg, useIt }) => {
@@ -22,6 +23,30 @@ const Layout = ({ children, headerBg, useIt }) => {
       <Header headerBg={headerBg} useIt={useIt} />
       <div>{children}</div>
       <Footer siteTitle={data.site.siteMetadata.title} />
+      <CookieConsent
+        location="bottom"
+        buttonText="Got it!"
+        style={{ background: "#f5f5f5", color: "#646467" }}
+        buttonStyle={{
+          background: "#646467",
+          color: "white",
+          fontSize: "13px",
+          borderRadius: "3px",
+          padding: "5px 20px",
+        }}
+        buttonClasses="btn btn-default btn-grey"
+        acceptOnScroll={true}
+      >
+        This website uses cookies.{" "}
+        <a
+          href="http://cookiesandyou.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-body border-bottom border-dark"
+        >
+          Learn More
+        </a>
+      </CookieConsent>
     </React.Fragment>
   )
 }
