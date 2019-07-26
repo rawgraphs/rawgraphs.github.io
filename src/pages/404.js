82635a68df0e5ca2d0b852f491fb2cf6dd963b78
uf0 <scnepa222@gmail.com>
2019-07-26 12:43:36 +0200

    added 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,20 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 py-5">
+          <h1>Ooooops!</h1>
+          <p>This page doesn't exist!</p>
+          <img
+            src="https://media.giphy.com/media/jR8EDxMbqi1QQ/giphy.gif"
+            className="img-fluid"
+            alt="pie charts"
+            width="100%"
+          />
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
